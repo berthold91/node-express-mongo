046d3ea5663104d97716491150ec150d80c00af8
berthold91 <bertholdperezdongmo@gmail.com>
2024-08-26 22:34:47 +0200

    url router created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,21 @@
+import Error404Page from "./pages/Error404Page.js";
 import HomePage from "./pages/HomePage.js";
-const products = document.getElementById("product-list");
-products.innerHTML = HomePage.render();
+import ProductPage from "./pages/ProductPage.js";
+import { parseRequestUrl } from "./utils.js";
+
+const routes = {
+    "/": HomePage,
+    "/#/products/:id": ProductPage
+};
+const router = () => {
+    const request = parseRequestUrl();
+    const parseUrl = ( request.resource ? `/${request.resource}` : '/' ) + 
+                     ( request.id ? ':id' : '') + 
+                     ( request.action ? `/${request.action}` : '');
+    const page = routes[parseUrl] ? routes[parseUrl] : Error404Page;
+    const main = document.getElementById('main');
+    main.innerHTML = page.render();
+};
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
---
+++ frontend/src/pages/ProductPage.js
@@ -0,0 +1,6 @@
+const ProductPage = {
+    render: () => {
+        return `<h1>this is the Product Page</h1>`;
+    }
+};
+export default ProductPage;
